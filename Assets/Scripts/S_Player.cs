891d76de337ad0e6ee5aea3f93f5dd95ef8ec3c6
anaha <anaharisohn@gmail.com>
2026-05-17 14:27:11 -0400

    Working on wall col

--- Assets/Scripts/S_Player.cs
+++ Assets/Scripts/S_Player.cs
@@ -6,6 +6,7 @@ public class S_Player : MonoBehaviour
     // VARAIBLES
 
     // Objects
+    public CapsuleCollider2D capsuleCol;
     public Rigidbody2D rb2d;
     public Animator animator;
     public SpriteRenderer spriteRen;
@@ -15,9 +16,16 @@ public class S_Player : MonoBehaviour
     public Vector2 moveSpeed = new Vector2(5, 1);
     public float jumpVel = 2f;
     public bool isFacingLeft { get; protected set; } = true;
+
+
+    // Physyic / raycast
+    public LayerMask groundLayer;
     public bool isGrounded { get; protected set; } = true;
 
-    public LayerMask groundLayer;
+    Vector2 edgeClipTopOrigin = Vector2.zero;
+    Vector2 edgeClipBotOrigin = Vector2.zero;
+    Vector2 edgeClipDirection = Vector2.zero;
+    float edgeClipRayDistance = .4f;
 
 
     /// <summary>
@@ -48,8 +56,31 @@ public class S_Player : MonoBehaviour
             isFacingLeft = moveDir.x < 0;
             spriteRen.flipX = isFacingLeft;
 
-            // Set move speed (horizontal) directly
-            rb2d.linearVelocityX = moveDir.x * moveSpeed.x;
+            // Check is player is hitting a wall
+            Vector2 centre = transform.position;
+            Vector2 extents = capsuleCol.bounds.extents;
+            float extentsX = isFacingLeft ? -extents.x : +extents.x;
+
+            edgeClipTopOrigin = centre + new Vector2(extentsX, extents.y);
+            edgeClipBotOrigin = centre + new Vector2(extentsX, -extents.y);
+
+            edgeClipDirection = new Vector2(extents.x, 0).normalized;
+            edgeClipRayDistance *= edgeClipDirection.x;
+
+            bool hitTop = Physics2D.Raycast(edgeClipTopOrigin, edgeClipDirection, edgeClipRayDistance, groundLayer);
+            bool hitBot = Physics2D.Raycast(edgeClipBotOrigin, edgeClipDirection, edgeClipRayDistance, groundLayer);
+
+            if(!hitBot && !hitBot) 
+            {
+                // Set move speed (horizontal) directly
+                rb2d.linearVelocityX = moveDir.x * moveSpeed.x;
+            }
+            
+            //fhuwihfwi
+
+            Debug.DrawLine(edgeClipTopOrigin, edgeClipTopOrigin + new Vector2(edgeClipRayDistance,0), hitTop ? Color.red: Color.green);
+            Debug.DrawLine(edgeClipBotOrigin, edgeClipBotOrigin + new Vector2(edgeClipRayDistance,0), hitBot ? Color.red: Color.green);
+            
         }
         animator.SetFloat("moveSpeedX", Mathf.Abs(moveDir.x));
 
@@ -58,8 +89,10 @@ public class S_Player : MonoBehaviour
 
         Vector2 rayOrigin = this.transform.position;
         Vector2 rayDir = Vector2.down;
-        float rayRange = 1.05f;
+        float rayRange = .3f;
         isGrounded = Physics2D.Raycast(rayOrigin, rayDir, rayRange, groundLayer);
+
+        animator.SetBool("isGrounded", isGrounded);
     }
 
 
@@ -68,6 +101,9 @@ public class S_Player : MonoBehaviour
     /// </summary>
     private void OnValidate()
     {
+        if (capsuleCol == null)
+            capsuleCol = GetComponent<CapsuleCollider2D>();
+
         if (rb2d == null)
             rb2d = GetComponent<Rigidbody2D>();
 
