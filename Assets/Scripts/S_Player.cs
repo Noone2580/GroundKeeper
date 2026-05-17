74f24135a61ca0488686ff4999d9646f1fed7e41
anaha <anaharisohn@gmail.com>
2026-05-16 21:17:36 -0400

    Added Game Files

---
+++ Assets/Scripts/S_Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class S_Player : MonoBehaviour
+{
+    // VARAIBLES
+
+    // Objects
+    public Rigidbody2D rb2d;
+    public Animator animator;
+    public SpriteRenderer spriteRen;
+
+    // Player
+    private Vector2 moveDir;
+    public Vector2 moveSpeed = new Vector2(5, 1);
+    public bool isFacingLeft { get; protected set; } = true;
+
+
+    /// <summary>
+    /// Triggers on Player InputAction Move and set moveDir to it's value.
+    /// </summary>
+    /// <param name="value"></param>
+    public void OnMove(InputValue value)
+    {
+        moveDir = value.Get<Vector2>();
+    }
+   
+
+    void Update()
+    {
+        if (Mathf.Abs(moveDir.x) > 0.1f)
+        {
+            // moveDir X is positive then is moving right
+            isFacingLeft = moveDir.x < 0;
+            spriteRen.flipX = isFacingLeft;
+
+            // Set move speed (horizontal) directly
+            rb2d.linearVelocityX = moveDir.x * moveSpeed.x;
+        }
+        //animator.SetFloat("moveSpeedX", Mathf.Abs(moveDir.x));
+
+    }
+
+
+    /// <summary>
+    /// Runs eveytime unity changes defalt vars
+    /// </summary>
+    private void OnValidate()
+    {
+        if (rb2d == null)
+            rb2d = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        if (spriteRen == null)
+            spriteRen = GetComponent<SpriteRenderer>();
+    }
+}
---
+++ Assets/Scripts/S_PlayerFollow.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class S_PlayerFollow : MonoBehaviour
+{
+    public Rigidbody2D target;
+    public SpriteRenderer targetSpriteRen;
+    public float heihtOffset;
+    public float lookAheadOffset;
+    public Vector2 lookAheadSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isFacingLeft = targetSpriteRen.flipX;
+        float offsetX = isFacingLeft ?-lookAheadOffset : +lookAheadOffset;
+
+        Vector3 targetPosition = target.position;
+
+
+        targetPosition.z = this.transform.position.z;
+
+        targetPosition.y += heihtOffset;
+
+        targetPosition.x += offsetX;
+
+        Vector3 newPosition = targetPosition;
+
+        newPosition.x = Mathf.Lerp(this.transform.position.x, targetPosition.x, Time.deltaTime * lookAheadSpeed.x);
+        newPosition.y = Mathf.Lerp(this.transform.position.y, targetPosition.y, Time.deltaTime * lookAheadSpeed.y);
+
+        this.transform.position = newPosition;
+    }
+
+    private void OnValidate()
+    {
+
+    }
+}
